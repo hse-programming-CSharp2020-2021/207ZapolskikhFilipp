13759ab55f4f1aab52c062489ee85125a1a50179
Keeper318 <fmzapolskikh@edu.hse.ru>
2020-12-03 13:03:28 +0400

    Revert "Revert """
    This reverts commit dcaf2f28cc0b8e0e0164f308f1fab9969dab2613.

---
+++ 02 module/Seminar2_11/homework/MyLib/Matrix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class Matrix
+    {
+        int[,] matrix;
+
+        public void MatrPrint()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write($"{matrix[i, j]} ");
+                Console.WriteLine();
+            }
+        }
+        public Matrix UnitMatr(int n)
+        {// Сформировать единичную матрицу:
+            if (n <= 0)
+                throw new ArgumentOutOfRangeException("Аргумент метода должен быть положительным!");
+            matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    matrix[i, j] = (i == j ? 1 : 0);
+            return this;
+        }
+    }
+}
